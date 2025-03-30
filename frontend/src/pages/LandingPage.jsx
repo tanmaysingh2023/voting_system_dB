6d35f2cc72ecd629ce0401bf87cddeaf69e003f9
ayush-705 <ayushdhimmar705@gmail.com>
2025-03-30 16:48:22 +0530

    Exp

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center px-6">
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col gap-10 items-center justify-center px-6">
       <header className="w-full flex justify-between items-center py-4 px-8 border-b border-gray-700">
         <h1 className="text-3xl font-bold">VoteNow</h1>
         <nav>
@@ -15,17 +15,22 @@ const LandingPage = () => {
         </nav>
       </header>
       
-      <main className="flex flex-col items-center text-center mt-16">
-        <h2 className="text-4xl font-extrabold">Secure & Transparent Voting System</h2>
-        <p className="text-gray-400 mt-4 max-w-2xl">
-          A modern and reliable online voting platform ensuring transparency, security, and accessibility for all users.
-        </p>
-        <button className="mt-6 px-6 py-3 bg-blue-600 rounded-lg text-lg font-semibold hover:bg-blue-500 transition">
-          Get Started
-        </button>
+      <main className="w-full flex items-center justify-center px-14">
+        <div className="flex flex-col items-center text-center mt-16">
+            <h2 className="text-4xl font-extrabold">Secure & Transparent Voting System</h2>
+            <p className="text-gray-400 mt-4 max-w-2xl">
+            A modern and reliable online voting platform ensuring transparency, security, and accessibility for all users.
+            </p>
+            <button className="mt-6 px-6 py-3 bg-blue-600 rounded-lg text-lg font-semibold hover:bg-blue-500 transition">
+            Get Started
+            </button>
+        </div>
+        {/* <div>
+            <img src="vote.png" alt="" />
+        </div> */}
       </main>
 
-      <section className="mt-16 grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
         <div className="p-6 bg-gray-800 rounded-lg shadow-lg">
           <h3 className="text-xl font-semibold">Secure Voting</h3>
           <p className="text-gray-400 mt-2">Blockchain technology ensures your vote remains safe and unaltered.</p>
